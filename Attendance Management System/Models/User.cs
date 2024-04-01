982a6a827ec7479e3340f150b0780ad16494bb07
Rahek <raheksalem@gmail.com>
2024-04-01 19:23:45 +0200

    user model

--- Attendance Management System/Models/User.cs
+++ Attendance Management System/Models/User.cs
@@ -7,5 +7,10 @@
         public string Email { get; set; }
         public string Password { get; set; }
         public string Phone { get; set; }
+        public virtual Student Student { get; set; }
+        public virtual Instructor Instructor { get; set; }
+        public virtual Employee Employee { get; set; }
+        public virtual Admin Admin { get; set; }
+        public virtual Supervisor Supervisor { get; set; }
     }
 }
