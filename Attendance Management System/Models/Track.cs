7a9220f168cfde0fc605ef9399e9e68d42561e0a
AhmedL3swy <Ahmed.a.alesawy@gmail.com>
2024-04-02 00:14:54 +0200

    Schedule Ef-Mapping

--- Attendance Management System/Models/Track.cs
+++ Attendance Management System/Models/Track.cs
@@ -2,6 +2,12 @@
 {
     public class Track
     {
+        public int Id { get; set; }
         public string Name { get; set; }
+
+        #region Navigation Properties
+        // 1-M relationship between Track and Schedule
+        public List<Schedule> Schedules { get; set; } = new List<Schedule>();
+        #endregion
     }
 }
