8c1a89e44480e5d7385b65a33d8e129bf298bf62
AhmedL3swy <Ahmed.a.alesawy@gmail.com>
2024-04-04 03:06:03 +0200

    Add Date and trackId properties to Permission model

--- Attendance Management System/Models/Permission.cs
+++ Attendance Management System/Models/Permission.cs
@@ -13,6 +13,8 @@ namespace Attendance_Management_System.Models
         public int ID { get; set; }
         public string Reason { get; set; }
 
+
+
         public string BodyOfDescription { get; set; }
 
         public PermissionStatus Status { get; set; } = PermissionStatus.Pending;
@@ -21,6 +23,10 @@ namespace Attendance_Management_System.Models
         [ForeignKey(nameof(Student))]
         public string StudentId { get; set; }
         public virtual Student Student { get; set; }
+
+        public DateOnly Date { get; set; }
+        public int trackId { get; set; }
+
 
 
     }
