80f43c1622aeaa5745bd02ba6520da8a05904885
AhmedL3swy <Ahmed.a.alesawy@gmail.com>
2024-04-04 02:59:53 +0200

    Add permissions functionality to InstructorController

--- Attendance Management System/Controllers/InstructorController.cs
+++ Attendance Management System/Controllers/InstructorController.cs
@@ -1,18 +1,79 @@
-﻿using Attendance_Management_System.Repos;
+﻿using Attendance_Management_System.Data;
+using Attendance_Management_System.Repos;
 using Microsoft.AspNetCore.Mvc;
-
+using Attendance_Management_System.Dtos;
+using Microsoft.EntityFrameworkCore;
+using Attendance_Management_System.Models;
 namespace Attendance_Management_System.Controllers
 {
     public class InstructorController : Controller
     {
+        itiContextSl db = new itiContextSl();
         IInstructorRepo InstructorRepo;
         public InstructorController(IInstructorRepo _InstructorRepo)
         {
             InstructorRepo = _InstructorRepo;
         }
-        public IActionResult Index()
+        public IActionResult Permissions()
         {
-            return View();
+            // Get the list of permissions from the database
+            var Permissions= db.Permissions.Where(p => p.Status == 0).Include(p => p.Student).ToList();
+            // Convert the list of permissions to a list of PermissionDto
+            List<PermissionDto> PermissionsDtos = new List<PermissionDto>();
+            foreach (var permission in Permissions)
+            {
+                PermissionsDtos.Add(new PermissionDto(permission));
+            }
+            
+            
+            return View(PermissionsDtos);
+        }
+
+
+
+        //[HttpPost("Permissions/Accept")]
+        [HttpPost]
+
+        public int Accept(int id)
+        {
+            try
+            {
+                // find the permission by id
+                var permission = db.Permissions.Find(id);
+                // update the status to accepted
+                permission.Status = PermissionStatus.Accepted;
+                // save the changes
+                db.SaveChanges();
+                // return fail or success status
+                return 1;
+            }
+            catch (Exception)
+            {
+                return 0;
+            }
+
+
+        }
+        // give special route name
+        //[HttpPost("Permissions/Reject")]
+        [HttpPost]
+        public int Reject(int id)
+        {
+            try
+            {
+                // find the permission by id
+                var permission = db.Permissions.Find(id);
+                // update the status to rejected
+                permission.Status = PermissionStatus.Refused;
+                // save the changes
+                db.SaveChanges();
+                // return fail or success status
+                return 1;
+            }
+            catch (Exception)
+            {
+                return 0;
+            }
         }
     }
 }
