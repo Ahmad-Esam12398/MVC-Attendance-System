a20bba1e54f0e4e4848108920a289e88dff33702
AhmedL3swy <Ahmed.a.alesawy@gmail.com>
2024-04-05 14:45:04 +0200

    Implement instructor permission functionality in IInstructorRepo.cs and InstructorRepo.cs

--- Attendance Management System/Repos/IInstructorRepo.cs
+++ Attendance Management System/Repos/IInstructorRepo.cs
@@ -1,6 +1,12 @@
-﻿namespace Attendance_Management_System.Repos
+﻿using Attendance_Management_System.Models;
+
+namespace Attendance_Management_System.Repos
 {
     public interface IInstructorRepo
     {
+        void AcceptPermission(int id);
+        void RefusePermission(int id);
+        List<Permission> getAllPermissions();
+        List<Permission> getPendingPermissions();
     }
 }
--- Attendance Management System/Repos/InstructorRepo.cs
+++ Attendance Management System/Repos/InstructorRepo.cs
@@ -1,4 +1,5 @@
 ﻿using Attendance_Management_System.Data;
+using Attendance_Management_System.Models;
 using Microsoft.EntityFrameworkCore;
 
 namespace Attendance_Management_System.Repos
@@ -9,6 +10,24 @@ namespace Attendance_Management_System.Repos
         public InstructorRepo(IitiContext _db)
         {
             db = _db;
+        }
+        public List<Permission> getAllPermissions()
+        {
+            return db.Permissions.ToList();
+        }
+        public void AcceptPermission(int id)
+        {
+            var permission = db.Permissions.Find(u => u.ID == id);
+            permission.Status = PermissionStatus.Accepted;
+        }
+        public void RefusePermission(int id)
+        {
+            var permission = db.Permissions.Find(u => u.ID == id);
+            permission.Status = PermissionStatus.Refused;
+        }
+        public List<Permission> getPendingPermissions()
+        {
+            return db.Permissions.Where(u => u.Status == PermissionStatus.Pending).ToList();
         }
     }
 }
