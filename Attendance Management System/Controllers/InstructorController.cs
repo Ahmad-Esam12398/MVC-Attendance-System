1824538876af509423e4c719f6f27ed306693ad6
AhmedL3swy <Ahmed.a.alesawy@gmail.com>
2024-04-05 14:42:14 +0200

    Add instructor permissions functionality to InstructorController.cs

--- Attendance Management System/Controllers/InstructorController.cs
+++ Attendance Management System/Controllers/InstructorController.cs
@@ -1,4 +1,6 @@
-﻿using Attendance_Management_System.Repos;
+﻿using Attendance_Management_System.Dtos;
+using Attendance_Management_System.Models;
+using Attendance_Management_System.Repos;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Attendance_Management_System.Controllers
@@ -13,6 +15,46 @@ namespace Attendance_Management_System.Controllers
         public IActionResult Index()
         {
             return View();
+        }
+        public IActionResult Permissions()
+        {
+            // Get the list of permissions from the database
+            var Permissions = InstructorRepo.getPendingPermissions();
+            // Convert the list of permissions to a list of PermissionDto
+            List<PermissionDto> PermissionsDtos = new List<PermissionDto>();
+            foreach (var permission in Permissions)
+            {
+                PermissionsDtos.Add(new PermissionDto(permission));
+            }
+            return View(PermissionsDtos);
+        }
+        [HttpPost]
+        public int Accept(int id)
+        {
+            try
+            {
+                InstructorRepo.AcceptPermission(id);
+
+                return 1;
+            }
+            catch (Exception)
+            {
+                return 0;
+            }
+        }
+        [HttpPost]
+        public int Reject(int id)
+        {
+            try
+            {
+                InstructorRepo.RefusePermission(id);
+
+                return 1;
+            }
+            catch (Exception)
+            {
+                return 0;
+            }
         }
     }
 }
