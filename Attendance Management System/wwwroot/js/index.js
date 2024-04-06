f5cf073120241e4be3c14b8288b2200697243554
AhmedL3swy <Ahmed.a.alesawy@gmail.com>
2024-04-06 20:08:00 +0200

    added schedule utilts in index.js

--- Attendance Management System/wwwroot/js/index.js
+++ Attendance Management System/wwwroot/js/index.js
@@ -4,6 +4,7 @@ var colarray = [0, 1, 2, 3, 4, 5];
 
 $(document).ready(function () {
     table = $('#myTable').DataTable({
+        responsive: true,
         orderCellsTop: true,
         fixedHeader: true,
         initComplete: function () {
@@ -11,6 +12,8 @@ $(document).ready(function () {
             createFilters();
         },
     });
+
+
 
     function createFilters() {
         // Date Filter
@@ -44,6 +47,7 @@ $(document).ready(function () {
 $(".btn").click(function () {
     var id = $(this).attr("id");
     var action = $(this).attr("action");
+    console.log(action, id);
     var clickedButton = $(this);
     $.ajax({
         url: action,
@@ -74,19 +78,66 @@ $(".btn").click(function () {
                 clickedButton.closest("tr").hide("slow");
 
             }
-        },
-        error: function (data) {
-            $.toast({
-                heading: 'Error',
-                text: 'An error occurred while processing your request.',
-                showHideTransition: 'slide',
-                icon: 'error'
-            })
-        }
-    });
+            else if (action == "/Instructor/Delete") {
+                $.toast({
+                    heading: 'Schedule Deleted',
+                    text: 'You have successfully deleted the Schedule.',
+                    showHideTransition: 'slide',
+                    icon: 'success'
+                });
+                clickedButton.closest("tr").hide("slow");
+            }
+
+
+            },
+            error: function (data) {
+                $.toast({
+                    heading: 'Error',
+                    text: 'An error occurred while processing your request.',
+                    showHideTransition: 'slide',
+                    icon: 'error'
+                })
+            }
+        });
 });
 $('#ClearDate').click(function () {
     $('#Date').val('');
     $('#Date').attr('title', '');
     api.columns(0).search('').draw();
+});
+const convertTime12to24 = (time12h) => {
+    const [time, modifier] = time12h.split(' ');
+
+    let [hours, minutes] = time.split(':');
+
+    if (hours === '12') {
+        hours = '00';
+    }
+
+    if (modifier === 'PM') {
+        hours = parseInt(hours, 10) + 12;
+    }
+
+    return `${hours}:${minutes}`;
+}
+$('.edit').click(function (event) {
+    var button = $(this);
+    var id = button.data('id');
+    var date = button.data('date');
+    var startTime = button.data('start-time');
+    var endTime = button.data('end-time');
+
+    // Parse time strings using Moment.js
+    var startTimeMoment = moment(startTime, 'hh:mm A');
+    var endTimeMoment = moment(endTime, 'hh:mm A');
+
+    // Format time in desired format (12-hour without AM/PM)
+    var formattedStartTime = startTimeMoment.format('HH:mm');
+    var formattedEndTime = endTimeMoment.format('HH:mm');
+
+    // Fill form fields with data
+    $('#EditId').val(id);
+    $('#EditDate').val(date);
+    $('#EditStartTime').val(formattedStartTime);
+    $('#EditEndTime').val(formattedEndTime);
 });
