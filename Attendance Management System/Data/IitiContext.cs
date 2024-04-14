d80f94e8ff7a64f8a3e487a88490f01546d316f0
Ahmad Esam <90632545+Ahmad-Esam12398@users.noreply.github.com>
2024-04-14 10:54:44 +0200

    Update IitiContext.cs

--- Attendance Management System/Data/IitiContext.cs
+++ Attendance Management System/Data/IitiContext.cs
@@ -1,20 +1,17 @@
 ﻿using Attendance_Management_System.Models;
-using Microsoft.EntityFrameworkCore;
-using System;
-using System.Collections.Generic;
 
 namespace Attendance_Management_System.Data
 {
     public interface IitiContext
     {
-        DbSet<Schedule> Schedules { get; set; }
-        DbSet<ScheduleEvent> ScheduleEvents { get; set; }
-        DbSet<Track> Tracks { get; set; }
-        DbSet<User> Users { get; set; } 
-
         List<Student> students { get; set; }
+        List<Track> Tracks { get; set; }
         List<ITIProgram> Programs { get; set; }
+        List<Permission> Permissions { get; set; }
+        List<Schedule> Schedules { get; set; }
         List<Attendance> Attendances { get; set; }
+        
+
 
     }
 }
