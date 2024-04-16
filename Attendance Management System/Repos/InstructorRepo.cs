9a20c749524180c2cb8aa5f5dfc941218a01f204
AhmedL3swy <Ahmed.a.alesawy@gmail.com>
2024-04-16 21:24:22 +0200

    updated Repo

--- Attendance Management System/Repos/InstructorRepo.cs
+++ Attendance Management System/Repos/InstructorRepo.cs
@@ -17,39 +17,57 @@ namespace Attendance_Management_System.Repos
         }
         public void AcceptPermission(int id)
         {
-            var permission = db.Permissions.Find( id);
+            var permission = db.Permissions.FirstOrDefault(p => p.ID == id);
             permission.Status = PermissionStatus.Accepted;
+            db.SaveChanges();
+
         }
         public void RefusePermission(int id)
         {
-            var permission = db.Permissions.Find(id);
+            var permission = db.Permissions.FirstOrDefault(p => p.ID == id);
             permission.Status = PermissionStatus.Refused;
+            db.SaveChanges();
         }
-        public List<Permission> getPendingPermissions()
+        public void RefusePermissionsNotToday()
         {
-            return db.Permissions.Where(u => u.Status == PermissionStatus.Pending).ToList();
+            var permissions = db.Permissions.Where(p => p.Date < DateOnly.FromDateTime(DateTime.Now)).ToList();
+            foreach (var permission in permissions)
+            {
+                permission.Status = PermissionStatus.Refused;
+            }
+            db.SaveChanges();
+        }
+        public List<Permission> getPendingPermissionsByTrackID(int TrackID)
+        {
+            RefusePermissionsNotToday();
+            return db.Permissions.Include(p => p.Student).Where(p => p.Status == PermissionStatus.Pending && p.Student.TrackID == TrackID).ToList();
         }
 
-        public List<Schedule> getSchedules()
+        public List<Schedule> getSchedulesByTrackID(int TrackID)
         {
-            return db.Schedules.ToList();
+            return db.Schedules.Where(s => s.TrackId == TrackID).ToList();
         }
 
         public void AddSchedule(Schedule Schedule)
         {
             Schedule.TrackId = 1;
             db.Schedules.Add(Schedule);
+            db.SaveChanges();
         }
         public void DeleteSchedule(int id)
         {
             var Schedule = db.Schedules.Find(id);
             db.Schedules.Remove(Schedule);
+            db.SaveChanges();
         }
 
         public void UpdateSchedule(Schedule schedule)
         {
-            var index = db.Schedules.Find(schedule.Id);
-            index = schedule;   
+            var Schedule = db.Schedules.FirstOrDefault(s => s.Id == schedule.Id);
+            Schedule.Date = schedule.Date;
+            Schedule.StartTime= schedule.StartTime;
+            Schedule.EndTime = schedule.EndTime;
+            db.SaveChanges();
         }
     }
 }
