c91d5bc74c28be9fcdf34eb74caf6a6c179eae8c
AhmedL3swy <Ahmed.a.alesawy@gmail.com>
2024-04-16 22:26:41 +0200

    Updated Repo To Fetch CurrentUser

--- Attendance Management System/Repos/InstructorRepo.cs
+++ Attendance Management System/Repos/InstructorRepo.cs
@@ -1,33 +1,44 @@
 ﻿using Attendance_Management_System.Data;
 using Attendance_Management_System.Models;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
+using System.Security.Claims;
 
 namespace Attendance_Management_System.Repos
 {
     public class InstructorRepo : IInstructorRepo
     {
         itiContext db;
-        public InstructorRepo(itiContext _db)
+        //UserManger Instance
+        private readonly UserManager<User> _userManager;
+        private readonly ClaimsPrincipal _userPrincipal; // Add a field to store the user principal
+
+        public InstructorRepo(itiContext _db, UserManager<User> userManager, ClaimsPrincipal userPrincipal)
         {
             db = _db;
+            _userManager = userManager;
+            _userPrincipal = userPrincipal; // Assign the user principal to the field
         }
+
         public List<Permission> getAllPermissions()
         {
             return db.Permissions.ToList();
         }
+
         public void AcceptPermission(int id)
         {
             var permission = db.Permissions.FirstOrDefault(p => p.ID == id);
             permission.Status = PermissionStatus.Accepted;
             db.SaveChanges();
-
         }
+
         public void RefusePermission(int id)
         {
             var permission = db.Permissions.FirstOrDefault(p => p.ID == id);
             permission.Status = PermissionStatus.Refused;
             db.SaveChanges();
         }
+
         public void RefusePermissionsNotToday()
         {
             var permissions = db.Permissions.Where(p => p.Date < DateOnly.FromDateTime(DateTime.Now)).ToList();
@@ -37,8 +48,10 @@ namespace Attendance_Management_System.Repos
             }
             db.SaveChanges();
         }
+
         public List<Permission> getPendingPermissionsByTrackID(int TrackID)
         {
+            var user = GetCurrentUser();
             RefusePermissionsNotToday();
             return db.Permissions.Include(p => p.Student).Where(p => p.Status == PermissionStatus.Pending && p.Student.TrackID == TrackID).ToList();
         }
@@ -54,6 +67,7 @@ namespace Attendance_Management_System.Repos
             db.Schedules.Add(Schedule);
             db.SaveChanges();
         }
+
         public void DeleteSchedule(int id)
         {
             var Schedule = db.Schedules.Find(id);
@@ -65,9 +79,14 @@ namespace Attendance_Management_System.Repos
         {
             var Schedule = db.Schedules.FirstOrDefault(s => s.Id == schedule.Id);
             Schedule.Date = schedule.Date;
-            Schedule.StartTime= schedule.StartTime;
+            Schedule.StartTime = schedule.StartTime;
             Schedule.EndTime = schedule.EndTime;
             db.SaveChanges();
+        }
+
+        public User GetCurrentUser()
+        {
+            return _userManager.GetUserAsync(_userPrincipal).Result; // Get the current user using the user principal
         }
     }
 }
