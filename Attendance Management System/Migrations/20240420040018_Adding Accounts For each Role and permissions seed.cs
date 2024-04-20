84851864566baf8665c230c770d0fc6f985be4d0
AhmedL3swy <Ahmed.a.alesawy@gmail.com>
2024-04-20 12:56:12 +0200

    upddated Migration and DB_intrailizer

--- Attendance Management System/Migrations/20240420040018_Adding Accounts For each Role and permissions seed.cs
+++ Attendance Management System/Migrations/20240420040018_Adding Accounts For each Role and permissions seed.cs
@@ -45,12 +45,12 @@ namespace Attendance_Management_System.Migrations
                 keyValues: new object[] { new DateOnly(2024, 4, 20), 5 },
                 column: "DateCreated",
                 value: new DateTime(2024, 4, 20, 6, 0, 17, 135, DateTimeKind.Local).AddTicks(4067));
-            migrationBuilder.Sql("update Users set Gender = 'M' where Id > 5");
-            migrationBuilder.Sql("update UsersRoles set RoleId = 1 where UserId = 5;");
-            migrationBuilder.Sql("update UsersRoles set RoleId = 2 where UserId = 3;");
-            migrationBuilder.Sql("insert into UsersRoles(RoleId, UserId) values(3, 3);");
-            migrationBuilder.Sql("update UsersRoles set RoleId = 4 where UserId = 6;");
-            migrationBuilder.Sql("update UsersRoles set RoleId = 5 where UserId = 1;");
+            //migrationBuilder.Sql("update Users set Gender = 'M' where Id > 5");
+            //migrationBuilder.Sql("update UsersRoles set RoleId = 1 where UserId = 5;");
+            //migrationBuilder.Sql("update UsersRoles set RoleId = 2 where UserId = 3;");
+            //migrationBuilder.Sql("insert into UsersRoles(RoleId, UserId) values(3, 3);");
+            //migrationBuilder.Sql("update UsersRoles set RoleId = 4 where UserId = 6;");
+            //migrationBuilder.Sql("update UsersRoles set RoleId = 5 where UserId = 1;");
 
         }
 
@@ -91,11 +91,11 @@ namespace Attendance_Management_System.Migrations
                 keyValues: new object[] { new DateOnly(2024, 4, 20), 5 },
                 column: "DateCreated",
                 value: new DateTime(2024, 4, 20, 5, 36, 14, 735, DateTimeKind.Local).AddTicks(9715));
-            migrationBuilder.Sql("update UsersRoles set RoleId = 6 where UserId = 5;");
-            migrationBuilder.Sql("update UsersRoles set RoleId = 6 where UserId = 3;");
-            migrationBuilder.Sql("delete from UsersRoles where UserId = 3 and RoleId = 3");
-            migrationBuilder.Sql("update UsersRoles set RoleId = 6 where UserId = 6;");
-            migrationBuilder.Sql("update UsersRoles set RoleId = 6 where UserId = 1;");
+            //migrationBuilder.Sql("update UsersRoles set RoleId = 6 where UserId = 5;");
+            //migrationBuilder.Sql("update UsersRoles set RoleId = 6 where UserId = 3;");
+            //migrationBuilder.Sql("delete from UsersRoles where UserId = 3 and RoleId = 3");
+            //migrationBuilder.Sql("update UsersRoles set RoleId = 6 where UserId = 6;");
+            //migrationBuilder.Sql("update UsersRoles set RoleId = 6 where UserId = 1;");
         }
     }
 }
